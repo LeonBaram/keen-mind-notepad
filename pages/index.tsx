1ac3b018c6a2bf51a08d098896ec643401b1452d
LeonBaram <leonbaram@protonmail.com>
2021-11-29 15:20:28 -0500

    Implement live data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
+// react
+import { useEffect, useRef, useState } from "react";
+// next
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+// firebase
+import { onValue, push } from "@firebase/database";
+import { dbref } from "../firebase-init";
+// styles
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState<string>("");
+  const [notes, setNotes] = useState<string[][]>([]);
+
+  useEffect(
+    () =>
+      onValue(dbref.Notes, (snapshot) =>
+        setNotes(Object.entries(snapshot.val()))
+      ),
+    []
+  );
 
   return (
     <div className={styles.container}>
@@ -14,13 +29,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <input
-          type="text"
-          name="user-input"
-          id="user-input"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
+        <ul>
+          {notes.map(([noteID, note]) => (
+            <li key={noteID}>{note}</li>
+          ))}
+        </ul>
+        <form
+          action="submit"
+          onSubmit={(e) => {
+            e.preventDefault();
+            push(dbref.Notes, input);
+            setInput("");
+          }}
+        >
+          <input
+            type="text"
+            name="user-input"
+            id="user-input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </form>
       </main>
     </div>
   );
